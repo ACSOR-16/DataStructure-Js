234d7e37147aaa1699ca85ba5638d77c967cb3b7
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-03-29 15:27:00 -0500

    creating an array with classse

---
+++ array.js
@@ -0,0 +1,44 @@
+// const array = ['oscar', 'wilfredo', 'alex el leon'];
+// array.push('liz');
+
+class MyArray{
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  //method
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.data;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  myDelete(index) {
+    const item = this.data[index];
+    this.shiftIndex(index);
+    return item;
+  }
+
+  shiftIndex(index){ 
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+
+}
+
+const array = new MyArray();
